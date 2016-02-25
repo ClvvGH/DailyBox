89a0ee25c32b69282192fb9087335df4488a3a53
邱大大 <qiu_1201w@yeah.net>
2016-02-25 10:18:13 +0800

    Initial Commit

---
+++ DailyBox/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DailyBox
+//
+//  Created by Clvv on 16/2/25.
+//  Copyright © 2016年 Clvv. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DailyBoxTests/DailyBoxTests.swift
@@ -0,0 +1,36 @@
+//
+//  DailyBoxTests.swift
+//  DailyBoxTests
+//
+//  Created by Clvv on 16/2/25.
+//  Copyright © 2016年 Clvv. All rights reserved.
+//
+
+import XCTest
+@testable import DailyBox
+
+class DailyBoxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
